d6e027ec320fc6248302d665a5c137c051ef857a
Faenya <claveau.alexis@gmail.com>
2018-12-21 12:06:41 +0100

    Message Class

--- src/main/java/tinyTwitt/Message.java
+++ src/main/java/tinyTwitt/Message.java
@@ -1,5 +1,21 @@
 package tinyTwitt;
 
+@Persistance.Capable(identityType=IdentityType.APPLICATION)
 public class Message {
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	Long id;
+	
+	@Persistent
+	String owner;
+	
+	@Persistent
+	String body;
+	
+	@Persistent
+	String picture;
+	
+	@Persistent
+	String date;
 
 }
