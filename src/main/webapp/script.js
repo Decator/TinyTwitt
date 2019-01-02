9c2c139cab688d6efc691afd90542230966fafae
maudv <m.vandorssen@agemos.fr>
2019-01-02 21:25:09 +0100

    Mise en place du html

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,10 +1,58 @@
 var app = angular.module('tinyTwitt', []).controller('Controller', ['$scope', '$window', function($scope, $window) {
+
+	$scope.messages = [{
+		id : 1,
+		sender : "sender",
+		owner : "owner",
+		body : "body",
+		date : new Date()
+	}, {
+		id : 2,
+		sender : "sender",
+		owner : "owner",
+		body : "body",
+		date : new Date()
+	}, {
+		id : 3,
+		sender : "sender",
+		owner : "owner",
+		body : "body",
+		date : new Date()
+	}];
 	
+	$scope.count = 4;
 	
+    $scope.myFunc = function() {
+        $scope.count++;
+    };
+    
+    $scope.addMessage = function(body) {
+    	console.log("add message");
+    	console.log(body);
+    	$scope.messages.push({
+			id : $scope.count,
+			sender : "sender",
+			owner : "owner",
+			body : body,
+			date : new Date()
+		});
+    	$scope.count++;
+    	console.log("ah");
+    }
+    
+    $scope.getTimeline = function() {
+    	
+    }
+    
+    $scope.followUser = function() {
+    	
+    }
 }]);
 
-function signOut() {
-	console.log("pourt");
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut();
+function onSignIn(){
+	console.log("signed in");
+}
+
+function onSignInFailure() {
+	console.log("sign in failed");
 }
